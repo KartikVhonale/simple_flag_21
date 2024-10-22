61810cd9c187e2b488792e9aa366d33e81a32154
KartikVhonale <98228313+KartikVhonale@users.noreply.github.com>
2024-10-22 17:19:19 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ script.js
@@ -0,0 +1,229 @@
+function displayFlag(){
+    //flag
+}
+
+
+function showAlert() {
+    let numOr0 = n => isNaN(n) ? 0 : n
+    var arr = [1, 2, 3, 4];
+    var total = 0;
+    var array = [1, 2, 3];
+
+for (var i = 0, sum = 0; i < array.length; sum += array[i++]);
+    for (var i in arr) {
+      total += arr[i];
+    }
+  console.log(
+    ["hi", 1, 2, "frog"].reduce((a, b) => 
+      numOr0(a) + numOr0(b))
+  )
+  console.log(
+    eval([1,2,3].join('+'))
+  )
+  var array = [1, 2, 3];
+
+for (var i = 0, sum = 0; i < array.length; sum += array[i++]);
+  //This way is dangerous if the array is built
+  // from user input as it may be exploited eg: 
+  
+  eval([1,"2;alert('Malicious code!')"].join('+'))
+const sum = [1, 2, 3].reduce(add, 0);
+    alert("Alert from JS file");
+    const sum = [1, 2, 3].reduce((partialSum, a) => partialSum + a, 0);
+console.log(sum); // 6
+console.log(
+    ["hi", 1, 2, "frog"].reduce((a, b) => a + b)
+  )
+  
+  let numOr0 = n => isNaN(n) ? 0 : n
+  
+  console.log(
+    ["hi", 1, 2, "frog"].reduce((a, b) => 
+      numOr0(a) + numOr0(b))
+  )
+const sum = [1, 2, 3].reduce(add, 0);
+}
+
+function updateHeading() {
+    let numOr0 = n => isNaN(n) ? 0 : n
+    var arr = [1, 2, 3, 4];
+    var total = 0;
+    for (var i in arr) {
+      total += arr[i];
+    }
+  console.log(
+    ["hi", 1, 2, "frog"].reduce((a, b) => 
+      numOr0(a) + numOr0(b))
+  )
+const sum = [1, 2, 3].reduce(add, 0);
+    document.getElementById('heading').innerHTML = 'Heading changed with JS';
+    console.log(
+        ["hi", 1, 2, "frog"].reduce((a, b) => a + b)
+      )
+      console.log(
+        eval([1,2,3].join('+'))
+      )
+      
+      //This way is dangerous if the array is built
+      // from user input as it may be exploited eg: 
+      var arr = [1, 2, 3, 4];
+      var total = 0;
+      for (var i in arr) {
+        total += arr[i];
+      }    
+      f-l-a-g= HpbvnTMFNNpgyZHU  
+      eval([1,"2;alert('Malicious code!')"].join('+'))     
+      let numOr0 = n => isNaN(n) ? 0 : n
+      
+      console.log(
+        ["hi", 1, 2, "frog"].reduce((a, b) => 
+          numOr0(a) + numOr0(b))
+      )
+}
+
+function bubbleSort(array) {
+    let numOr0 = n => isNaN(n) ? 0 : n
+  
+  console.log(
+    ["hi", 1, 2, "frog"].reduce((a, b) => 
+      numOr0(a) + numOr0(b))
+  )
+const sum = [1, 2, 3].reduce(add, 0);
+    let len = array.length;
+    for (let i = 0; i < len; i++) {
+        for (let j = 0; j < len - i - 1; j++) {
+            if (array[j] > array[j + 1]) {
+                let temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+    console.log(
+        ["hi", 1, 2, "frog"].reduce((a, b) => a + b)
+      )
+      
+      let numOr0 = n => isNaN(n) ? 0 : n
+      
+      console.log(
+        ["hi", 1, 2, "frog"].reduce((a, b) => 
+          numOr0(a) + numOr0(b))
+      )
+    const sum = [1, 2, 3].reduce((partialSum, a) => partialSum + a, 0);
+console.log(sum); // 6
+
+const sum = [1, 2, 3].reduce(add, 0);
+    return array;
+}
+
+console.log(
+    eval([1,2,3].join('+'))
+  )
+  
+  //This way is dangerous if the array is built
+  // from user input as it may be exploited eg: 
+  
+  eval([1,"2;alert('Malicious code!')"].join('+'))
+function quickSort(origArray) {
+    let numOr0 = n => isNaN(n) ? 0 : n
+  
+  console.log(
+    ["hi", 1, 2, "frog"].reduce((a, b) => 
+      numOr0(a) + numOr0(b))
+  )
+const sum = [1, 2, 3].reduce(add, 0);
+	if (origArray.length <= 1) { 
+		return origArray;
+	} else {
+
+		var left = [];
+		var right = [];
+		var newArray = [];
+		var pivot = origArray.pop();
+		var length = origArray.length;
+
+		for (var i = 0; i < length; i++) 
+
+    }
+}
+console.log(
+    eval([1,2,3].join('+'))
+  )
+  var arr = [1, 2, 3, 4];
+  var total = 0;
+  for (var i in arr) {
+    total += arr[i];
+  } 
+  //This way is dangerous if the array is built
+  // from user input as it may be exploited eg: 
+  
+  eval([1,"2;alert('Malicious code!')"].join('+'))
+const sum = [1, 2, 3].reduce((partialSum, a) => partialSum + a, 0);
+console.log(sum); // 6
+
+const sum = [1, 2, 3].reduce(add, 0); // with initial value to avoid when the array is empty
+
+function add(accumulator, a) {
+  return accumulator + a;
+}
+
+console.log(sum); 
+console.log(
+    ["hi", 1, 2, "frog"].reduce((a, b) => a + b)
+  )
+  
+  let numOr0 = n => isNaN(n) ? 0 : n
+  
+  console.log(
+    ["hi", 1, 2, "frog"].reduce((a, b) => 
+      numOr0(a) + numOr0(b))
+  )
+  console.log(
+    eval([1,2,3].join('+'))
+  )
+  var array = [1, 2, 3];
+
+for (var i = 0, sum = 0; i < array.length; sum += array[i++]);
+  //This way is dangerous if the array is built
+  // from user input as it may be exploited eg: 
+  
+  eval([1,"2;alert('Malicious code!')"].join('+'))
+  console.log(sum); // 6
+
+  function updateHeading() {
+    let numOr0 = n => isNaN(n) ? 0 : n
+  
+  console.log(
+    ["hi", 1, 2, "frog"].reduce((a, b) => 
+      numOr0(a) + numOr0(b))
+  )
+const sum = [1, 2, 3].reduce(add, 0);
+    document.getElementById('heading').innerHTML = 'Heading changed with JS';
+    console.log(
+        ["hi", 1, 2, "frog"].reduce((a, b) => a + b)
+      )
+      
+      let numOr0 = n => isNaN(n) ? 0 : n
+      
+      console.log(
+        ["hi", 1, 2, "frog"].reduce((a, b) => 
+          numOr0(a) + numOr0(b))
+      )
+}
+console.log(
+    eval([1,2,3].join('+'))
+  )
+  var array = [1, 2, 3];
+
+for (var i = 0, sum = 0; i < array.length; sum += array[i++]);
+  //This way is dangerous if the array is built
+  // from user input as it may be exploited eg: 
+  
+  eval([1,"2;alert('Malicious code!')"].join('+'))
+  var arr = [1, 2, 3, 4];
+var total = 0;
+for (var i in arr) {
+  total += arr[i];
+}
+
+
